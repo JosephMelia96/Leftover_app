a3c3fc7706d0228bad812c8f47d4de79351811c2
arslankhurram <marslankhurram@gmail.com>
2019-11-25 03:43:48 -0500

    Added: -Navigation Drawer -Fragments for navigation menu items -Search bar

--- app/src/main/java/com/example/bcs421_leftoversapp/HomeActivity.java
+++ app/src/main/java/com/example/bcs421_leftoversapp/HomeActivity.java
@@ -1,10 +1,16 @@
 package com.example.bcs421_leftoversapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,12 +24,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
-    TextView name, email, id;
-    ImageView googlePhoto;
-
+    private DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    View headerView;
 
     GoogleSignInClient mGoogleSignInClient;
 
@@ -39,28 +46,44 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         initViews();
         showUserInfo(acct);
-    }
 
-    private void showUserInfo(GoogleSignInAccount acct) {
-        if (acct != null) {
-            String personName = acct.getDisplayName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
-            Uri personPhoto = acct.getPhotoUrl();
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
 
-            name.setText(personName);
-            email.setText(personEmail);
-            id.setText(personId);
-            Glide.with(this).load(String.valueOf(personPhoto)).into(googlePhoto);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_open);
+        drawerLayout.addDrawerListener(toggle);// add listener for navigation drawer
+        toggle.syncState();
+
+        //open home fragment on start
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
         }
     }
 
     private void initViews() {
-        name = findViewById(R.id.userName);
-        email = findViewById(R.id.email);
-        id = findViewById(R.id.userId);
-        googlePhoto = findViewById(R.id.userPhoto);
-        findViewById(R.id.signOutButton).setOnClickListener(this);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        headerView = navigationView.getHeaderView(0);
+    }
+
+    //update labels in nav header with user info
+    private void showUserInfo(GoogleSignInAccount acct) {
+        if (acct != null) {
+            TextView nav_username = headerView.findViewById(R.id.nav_userName); //access item by through navHeader
+            TextView nav_userEmail = headerView.findViewById(R.id.nav_userEmail);
+            ImageView nav_userPicture = headerView.findViewById(R.id.nav_userPicture);
+
+            nav_username.setText(acct.getDisplayName());
+            nav_userEmail.setText(acct.getEmail());
+            Glide.with(this).load(String.valueOf(acct.getPhotoUrl())).into(nav_userPicture);
+        }
     }
 
     private void signOut() {
@@ -74,13 +97,47 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.signOutButton:
+            case R.id.nav_signOut:
                 signOut();
                 break;
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) { //use switch to read what item is pressed and switch fragment accordingly
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_accountSettings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AccountSettingsFragment()).commit();
+                break;
+            case R.id.nav_savedRecipe:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SavedRecipesFragment()).commit();
+                break;
+            case R.id.nav_signOut:
+                signOut();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START); //close navigation drawer after selecting item
+        return true;
+    }
+
+    //close nav drawer on back press if open
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/bcs421_leftoversapp/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.bcs421_leftoversapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    EditText searchBar;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+
+        searchBar = v.findViewById(R.id.search);
+
+        return v;
+
+    }
+}
