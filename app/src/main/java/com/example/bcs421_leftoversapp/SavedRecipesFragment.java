eaa36307788908a5d8ab656f35db8e9661555018
arslankhurram <marslankhurram@gmail.com>
2019-11-25 03:43:48 -0500

    Added: -Navigation Drawer -Fragments for navigation menu items -Search bar

---
+++ app/src/main/java/com/example/bcs421_leftoversapp/SavedRecipesFragment.java
@@ -0,0 +1,18 @@
+package com.example.bcs421_leftoversapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SavedRecipesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_saved_recipes, container, false);
+    }
+}
