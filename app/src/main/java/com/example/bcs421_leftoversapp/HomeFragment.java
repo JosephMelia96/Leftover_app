9fec7cce19ad04b399eabdbf4c132320acdc0e64
arslankhurram <marslankhurram@gmail.com>
2019-11-25 16:15:38 -0500

    Added code to pull and display api result

--- app/src/main/java/com/example/bcs421_leftoversapp/HomeFragment.java
+++ app/src/main/java/com/example/bcs421_leftoversapp/HomeFragment.java
@@ -6,15 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class HomeFragment extends Fragment {
 
     EditText searchBar;
+    TextView apiOutput;
 
     @Nullable
     @Override
@@ -23,6 +35,39 @@ public class HomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home,container,false);
 
         searchBar = v.findViewById(R.id.search);
+        apiOutput = v.findViewById(R.id.apiText);
+
+        final OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .url("https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%252Cgarlic&q=omelet")
+                .get()
+                .addHeader("x-rapidapi-host", "recipe-puppy.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "d80f17b0admsh0a6a81c192d0d1dp19d544jsn192a4c9b69db")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    response = client.newCall(request).execute();
+                    final String myResponse = response.body().string();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            apiOutput.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+
 
 
         return v;
