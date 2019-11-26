45a02e3817e42b7530917f89659280641b6cb92b
arslankhurram <marslankhurram@gmail.com>
2019-11-25 22:42:56 -0500

    Added searchBar and grid layout

--- app/src/main/java/com/example/bcs421_leftoversapp/HomeFragment.java
+++ app/src/main/java/com/example/bcs421_leftoversapp/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.bcs421_leftoversapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,22 +11,26 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class HomeFragment extends Fragment {
 
-    EditText searchBar;
+    SearchView searchBar;
     TextView apiOutput;
 
     @Nullable
@@ -37,34 +42,12 @@ public class HomeFragment extends Fragment {
         searchBar = v.findViewById(R.id.search);
         apiOutput = v.findViewById(R.id.apiText);
 
-        final OkHttpClient client = new OkHttpClient();
-
-        final Request request = new Request.Builder()
-                .url("https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%252Cgarlic&q=omelet")
-                .get()
-                .addHeader("x-rapidapi-host", "recipe-puppy.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", "d80f17b0admsh0a6a81c192d0d1dp19d544jsn192a4c9b69db")
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
+        searchBar.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
-            }
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    response = client.newCall(request).execute();
-                    final String myResponse = response.body().string();
-
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            apiOutput.setText(myResponse);
-                        }
-                    });
-                }
+                apiOutput.setText(searchBar.getQuery().toString());
+                return true;
             }
         });
 
