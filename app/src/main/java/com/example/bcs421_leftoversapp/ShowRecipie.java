4959d346ca5a94de5d21ebcbffe5a3cff7c16c51
arslankhurram <marslankhurram@gmail.com>
2019-12-02 03:08:42 -0500

    added share feature

--- app/src/main/java/com/example/bcs421_leftoversapp/ShowRecipie.java
+++ app/src/main/java/com/example/bcs421_leftoversapp/ShowRecipie.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-public class ShowRecipie extends AppCompatActivity {
+public class ShowRecipie extends AppCompatActivity implements View.OnClickListener {
 
     TextView ingredients, title;
     ImageView img;
@@ -23,6 +24,9 @@ public class ShowRecipie extends AppCompatActivity {
         ingredients=findViewById(R.id.ingredients);
         title = findViewById(R.id.title);
         img = findViewById(R.id.img);
+        findViewById(R.id.btn_share).setOnClickListener(this);
+
+
         String recIngr = getIntent().getStringExtra("ingr");
         String recTitle = getIntent().getStringExtra("title");
         String recImg = getIntent().getStringExtra("img");
@@ -39,4 +43,17 @@ public class ShowRecipie extends AppCompatActivity {
         Intent toSite = new Intent(Intent.ACTION_VIEW, Uri.parse(recHref));
         startActivity(toSite);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        String shareSubject = "Check This Delicious Recipe Out!";
+        String shareBody = getIntent().getStringExtra("title") +
+                "\n\n" + getIntent().getStringExtra("href");
+        intent.putExtra(Intent.EXTRA_SUBJECT,shareSubject);
+        intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(intent,"Share Using"));
+    }
+
 }
