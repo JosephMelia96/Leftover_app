a44a494bb7bfac7dc761b7830063fde5070c370b
JosephMelia96 <melij@farmingdale.edu>
2019-12-05 17:59:16 -0500

    Remove unused import in showrecipes

--- app/src/main/java/com/example/bcs421_leftoversapp/ShowRecipe.java
+++ app/src/main/java/com/example/bcs421_leftoversapp/ShowRecipe.java
@@ -6,7 +6,6 @@ import androidx.core.content.FileProvider;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
